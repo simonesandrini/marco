92dc5ef9d9392cd1098cfef0da6bd614cbab3cc5
simonesandrini <114070276+simonesandrini@users.noreply.github.com>
2022-09-21 12:52:47 +0200

    Add files via upload

---
+++ albero.cpp
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+int a,b,c,d,e,f;
+a=1;
+b=1;
+c=1;
+d=1;
+e=1;
+f=1;
+printf("Oh che bel castello marcondirondirondello, oh che bel castello marcondirondirondà Il mio è ancora più bello marcondirondirondello, il mio è ancora più bello marcondirondirondà");
+
+printf("E noi lo ruberemo marcondirondirondello, e noi lo ruberemo marcondirondirondà E noi lo rifaremo marcondirondirondello, e noi lo rifaremo marcondirondirondà");
+
+printf("E noi lo bruceremo marcondirondirondello, e noi lo bruceremo marcondirondirondà E noi lo spegneremo marcondirondirondello, e noi lo spegneremo marcondirondirondà\n");
+printf("Sparerem cannoni marcondirondirondello, Sparerem cannoni marcondirondirondà Spareremo i razzi marcondirondirondello, Spareremo i razzi marcondirondirondà\n");
+printf(" ");
+do
+{
+printf("*");
+a++;
+}
+while (a<=1);
+printf("\n");
+printf(" ");
+do
+{
+printf("*");
+b++;
+}
+while (b<=3);
+printf("\n");
+printf(" ");
+do
+{
+printf("*");
+c++;
+}
+while (c<=5);
+printf("\n");
+printf(" ");
+do
+{
+printf("*");
+d++;
+}
+while (d<=7);
+printf("\n");
+do
+{
+printf(" *\n");
+e++;
+}
+while (e<=2);
+printf(" ");
+do
+{
+printf("*");
+f++;
+}
+while (f<=3);
+printf("\n");
+system("PAUSE");
+return 0;
+}
+
